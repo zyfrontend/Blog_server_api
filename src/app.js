a27df66ac0258317ebb5c3dcead64c49e79565fc
zyfrontend <frontendzy@gmail.com>
2022-02-23 16:47:39 +0800

    commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const Koa = require("koa");
+
+const { APP_PORT } = require("./config/config.default");
+
+const app = new Koa();
+
+app.use((ctx, next) => {
+  ctx.body = "hello api";
+});
+
+app.listen(APP_PORT, () => {
+  console.log(`server is running on http://localhost:${APP_PORT}`);
+});
